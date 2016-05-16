23d9ff1c0a68592ca5b66de0b1f3df1af8c82b4a
Florenz Erstling <florenz.erstling@gmx.de>
2016-05-16 11:28:57 +0200

    reorganise trainer    working

--- js/controllers/simple-trainer-controller.js
+++ js/controllers/simple-trainer-controller.js
@@ -3,9 +3,25 @@ angular.module('vocabTrainer').controller('SimpleTrainerController', function ($
     $scope.flashCard = Vocab.currentFlashCard;
     $scope.displayAnswer = false;
 
-    $scope.seeAnswer = function () {
-        $scope.displayAnswer = true;
-    }
+
+    $scope.iniTrainer = function () {
+        Vocab.chargeVocs(this.flashCards);
+        Vocab.iniTrainer();
+        this.flashCard = Vocab.currentFlashCard;
+        this.displayAnswer = false;
+    };
+
+    $scope.setEnterAction = function (userAnswer) {
+        console.log($scope.displayAnswer);
+        if ($scope.displayAnswer === true) {
+            console.log("deny");
+            return $scope.denyAnswer();
+        }
+        if ($scope.displayAnswer === false) {
+            console.log("check");
+            return $scope.checkAnswer(userAnswer);
+        }
+    };
     $scope.checkAnswer = function (userAnswer) {
         if (Vocab.checkAnswer(userAnswer)){
           $scope.displayAnswer = true;
@@ -18,50 +34,31 @@ angular.module('vocabTrainer').controller('SimpleTrainerController', function ($
 
     $scope.acceptAnswer = function(){
         Vocab.markAnswerAsCorrect();
-        proceedToNextFlashCard();
+        $scope.proceedToNextFlashCard();
     }
     $scope.denyAnswer = function(){
         Vocab.markAnswerAsWrong();
-        proceedToNextFlashCard();
+        $scope.proceedToNextFlashCard();
     }
     $scope.showAnswer = function(){
         $scope.displayAnswer = true;
     }
 
-    var proceedToNextFlashCard = function () {
+    $scope.proceedToNextFlashCard = function () {
+        $("#user_answer").val("");
         $scope.flashCard = Vocab.currentFlashCard;
         $scope.displayAnswer = false;
     };
 
     $scope.soon = function (word) {
         Vocab.markForSoon(word);
-        proceedToNextFlashCard();
+        $scope.proceedToNextFlashCard();
     };
 
     $scope.later = function (word) {
         Vocab.markForLater(word);
-        proceedToNextFlashCard();
-    }
-    $scope.keyUpCheckAnswer = function (userAnswer) {
-        event.preventDefault();
-        if(event.keyCode == 13){
-            console.log("jojo");
-            console.log(userAnswer);
-            $scope.checkAnswer(userAnswer);
-
-        }
+        $scope.proceedToNextFlashCard();
     };
-    $scope.setEnterAction = function (userAnswer) {
-        console.log($scope.displayAnswer);
-        if ($scope.displayAnswer === true) {
-            console.log("deny");
-            return $scope.denyAnswer();
-        }
-        if ($scope.displayAnswer === false) {
-            console.log("check");
-            return $scope.checkAnswer(userAnswer);
-        }
-    }
 }).directive('normalFlashCard', function() {
     return {
         restrict: 'E',
